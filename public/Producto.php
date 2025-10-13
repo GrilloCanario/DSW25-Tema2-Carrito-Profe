a90351b28033b2ca49eb5a7d66e476219907c1a0
Andrés Crespo <acrerosj@canariaseducacion.es>
2025-10-13 17:53:06 +0000

    propiedad id readonly

--- public/Producto.php
+++ public/Producto.php
@@ -1,10 +1,21 @@
 <?php
 
 class Producto {
-    public string $nombre;
-    public float $precio;
+    // public string $nombre;
+    // public float $precio;
+
+    // public function __construct(string $nombre, float $precio) {
+    //     $this->nombre = $nombre;
+    //     $this->precio = $precio;
+    // }
+
+    public function __construct(
+        public readonly string $id,
+        public string $nombre,
+        public float $precio
+    ) {}
 
     public function obtenerInfo(): string {
-        return sprintf("Producto: %s, Precio: %.2f€", $this->nombre, $this->precio);
+        return sprintf("Id: %s, Producto: %s, Precio: %.2f€", $this->id, $this->nombre, $this->precio);
     }
 }
--- public/prueba-producto.php
+++ public/prueba-producto.php
@@ -1,15 +1,15 @@
 <?php
 require_once 'Producto.php';
 
-$libro = new Producto();
+$libro = new Producto("php123", "Apuntes de PHP", 1.75);
 
 echo "<pre>";
 var_dump($libro);
 echo "</pre>";
 $libro2 = $libro;  // Se referencia al mismo objeto.
 
-$libro->nombre = "Apuntes de PHP";
-$libro2->precio = 0.99;
+//$libro->nombre = "Apuntes de PHP";
+//$libro2->precio = 0.99;
 
 echo "<pre>";
 var_dump($libro2);
@@ -18,12 +18,14 @@ echo $libro->obtenerInfo();
 
 echo "<hr>";
 
-$raton = new Producto();
-$raton->nombre = "Ratón USB con rueda";
-$raton->precio = 5.9;
+$raton = new Producto("raton666", "Raton USB con rueda", 7);
+//$raton->nombre = "Ratón USB con rueda";
+//$raton->precio = 5.9;
 
 echo "<pre>";
 var_dump($raton);
 echo "</pre>";
 
+//$raton->id = "raton222";
 echo $raton->obtenerInfo();
+
